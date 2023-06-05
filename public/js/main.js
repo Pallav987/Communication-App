6631c9d71def894d45bcb95f7cc72eaa3ae6a54d
Pallav987 <pallavvarshney76@gmail.com>
2023-06-05 09:29:01 +0530

    add chatMessage

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,37 @@
 const socket = io();
 const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
 
+// Message from server
 socket.on('message',message =>{
    console.log(message);
+   outputMessage(message);
+
+   // Scroll Down
+   ChatMessages.scrollTop = chatMessages.scrollHeight;
 });
 
 chatForm.addEventListener('submit',(e)=>{
   e.preventDefault();
-  const msg = e.target.elments.msg.value;
+  const msg = e.target.elements.msg.value;
+// Emit message to server
+  socket.emit('chatMessage',msg);
 
-  console.log(msg);
+  // clear Input
+  e.target.elements.msg.value =  '';
+  e.target.elements.msg.focus();
+
 })
+
+// Output message to Dom
+
+function outputMessage(message) {
+  const div = document.createElement('div');
+  div.classList.add('message');
+  div.innerHTML = `<p class='meta'>Pallav<span>9:12pm</span></p>
+  <p class='text'>
+      ${message}
+  </p>`;
+  document.querySelector('.chat-messages').appendChild(div);    
+
+}
